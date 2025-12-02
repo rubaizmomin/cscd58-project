1d6dcf71086006c230899cdf952c4ffa02652ccf
None <None>
2025-12-02 05:46:59 -0800

    merge extraction logic from mininet to adj list

--- src/extractions/convert_mininet_to_py.py
+++ src/extractions/convert_mininet_to_py.py
@@ -1,6 +1,7 @@
 from mininet.net import Mininet
-from mininet.node import OVSSwitch, Host, OVSController, Node
-from mininet.link import TCLink, Intf # this library makes it more realistic with metrics like bamdwidth, delay, packet loss
+from mininet.node import OVSSwitch, Host, OVSController, Node, Switch
+from mininet.link import TCLink, Intf # this library makes it more realistic with metrics like bandwidth, delay, packet loss
+from failures.lock import mininet_cmd_lock
 
 # failed links will return infinity
 def parse_ping(s: str) -> float:
@@ -15,22 +16,50 @@ def extract_link(h1: Node, h2:  Node) -> float:
     assert h1 is not h2
     assert len(h1.connectionsTo(h2)) > 0
     cmdStr = "ping -c 1 " + h2.IP()
-    pingStr = h1.cmd(cmdStr)
+    with mininet_cmd_lock:
+        pingStr = h1.cmd(cmdStr)
     return  parse_ping(pingStr)# parse_ping(pingStr)
-
 
 # gets takes all the links in net and passes a ping through 
 def extract_mininet(net: Mininet) -> dict:
     weights = {}
-    for s1 in net.switches:
-        for h2 in net.hosts:
-            if len(s1.connectionsTo(h2)) > 0:
-                weights[s1, h2] = extract_link(s1, h2)
-            
+    for node in net.hosts + net.switches:
+        weights[node.name] = {}
+
+    for link in net.links:
+        if1 = link.intf1
+        if2 = link.intf2
+
+        # prevent race conditions with random failures by locking the thread
+        with mininet_cmd_lock:
+            up1 = if1.isUp()
+            up2 = if2.isUp()
+
+        # if links are down then they dont share an edge, basically inf
+        if not up1 or not up2:
+            continue
         
-        for s2 in net.switches:
-            if s1 is not s2 and len(s1.connectionsTo(s2)) > 0:
-                weights[s1, s2] = extract_link(s1, s2)
+        n1 = if1.node.name
+        n2 = if2.node.name
+        
+        # check because switch to host is not possible to ping
+        isHost1   = isinstance(if1.node, Host)
+        isHost2   = isinstance(if2.node, Host)
 
+        # assign the value from switch to host the same as host to switch
+        if isHost1 and not isHost2:
+            rtt = extract_link(if1.node, if2.node)
+            weights[n1][n2] = rtt
+            weights[n2][n1] = rtt
+            continue
 
+        if isHost2 and not isHost1:
+            rtt = extract_link(if2.node, if1.node)
+            weights[n1][n2] = rtt
+            weights[n2][n1] = rtt
+            continue
+        
+        weights[if1.node.name][if2.node.name] = extract_link(if1.node, if2.node)
+        weights[if2.node.name][if1.node.name] = extract_link(if2.node, if1.node)
+        
     return weights
--- src/simulation/simulation.py
+++ src/simulation/simulation.py
@@ -2,7 +2,7 @@ import threading
 import time
 from extractions.convert_nx_to_mininet import convert_nx_to_mininet
 from failures.link_failures import random_failures
-from extractions.convert_mininet_to_nx import convert_mininet_to_nx
+from extractions.convert_mininet_to_py import extract_mininet
 from algorithms.dijkstras import dijkstras, reconstructPath
 from algorithms.bellmanFord import bellmanFord
 import networkx as nx
@@ -11,41 +11,44 @@ def run_simulation(nx_graph):
 
     source = "h1"
     target = "h3"
-    
-    # get the mininet graph and start the mininet graph
-    net = convert_nx_to_mininet(nx_graph)
-    net.start()
+    try:
+        # get the mininet graph and start the mininet graph
+        net = convert_nx_to_mininet(nx_graph)
+        net.start()
 
-    # start random link failures using a thread
-    link_failure_thread = threading.Thread(
-        target=random_failures,
-        args=(net,),
-        kwargs={'fail_prob':0.3, 'recover_prob':0.3, 'sleep_interval':5},
-        daemon=True
-    )
-    link_failure_thread.start()
+        # start random link failures using a thread
+        link_failure_thread = threading.Thread(
+            target=random_failures,
+            args=(net,),
+            kwargs={'fail_prob':0.2, 'recover_prob':0.5, 'sleep_interval':5},
+            daemon=True
+        )
+        link_failure_thread.start()
 
-    while True:
-        
-        time.sleep(5)
+        while True:
+            time.sleep(5)
 
-        # after running the mininet, get the current state of the graph in networkx
-        adj = convert_mininet_to_nx(net)
+            # after running the mininet, get the current state of the graph in networkx
+            adj = extract_mininet(net)
+            print(adj)
+            # run dijkstra
+            dijkstra_dist, dijkstra_prev = dijkstras(adj, source)
+            dijkstra_path = reconstructPath(dijkstra_prev, source, target)
 
-        # run dijkstra
-        dijkstra_dist, dijkstra_prev = dijkstras(adj, source)
-        dijkstra_path = reconstructPath(dijkstra_prev, source, target)
+            # run bellman ford
+            bf_dist, bf_prev, bf_negativeCycle = bellmanFord(adj, source)
+            if bf_negativeCycle:
+                bf_path = []
+                bf_cost = float('inf')
+                print("Negative weight cycle detected")
+            else:
+                bf_path = reconstructPath(bf_prev, source, target)
+                bf_cost = bf_dist.get(target, float('inf'))
 
-        # run bellman ford
-        bf_dist, bf_prev, bf_negativeCycle = bellmanFord(adj, source)
-        if bf_negativeCycle:
-            bf_path = []
-            bf_cost = float('inf')
-            print("Negative weight cycle detected")
-        else:
-            bf_path = reconstructPath(bf_prev, source, target)
-            bf_cost = bf_dist.get(target, float('inf'))
-
-        print("\n==== CURRENT TOPOLOGY =====")
-        print("Dijkstra path:", dijkstra_path, "cost=", dijkstra_dist[target])
-        print("Bellman-Ford path:", bf_path, "cost=", bf_cost)
+            print("\n==== CURRENT TOPOLOGY =====")
+            print("Dijkstra path:", dijkstra_path, "cost=", dijkstra_dist[target])
+            print("Bellman-Ford path:", bf_path, "cost=", bf_cost)
+    except KeyboardInterrupt:
+        print("\nStopping the simulation and cleaning mininet")
+    finally:
+        net.stop()
